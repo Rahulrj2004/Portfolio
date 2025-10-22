406a58c3f3ab12678f3d4b4e207d0b57ca5a134f
rahulrj2004 <r2004jawalkar@gmail.com>
2025-10-22 17:46:36 +0530

    Added the links to the projects

--- src/constants/index.js
+++ src/constants/index.js
@@ -12,7 +12,7 @@ export const myProjects = [
             " Virtual scrolling for seamless content loading"
             , "Profile view with user-specific content 👤"
         ],
-        href: "",
+        href: "x-tweets-production.up.railway.app",
         logo: "",
         image: "/assets/projects/Twitter.jpeg",
         tags: [
@@ -48,7 +48,7 @@ export const myProjects = [
             " Click on any movie card to watch its trailer instantly through a fullscreen model 🎥",
             "Sleek and responsive user interface ",
         ],
-        href: "",
+        href: "https://github.com/Rahulrj2004/recommendation_system",
         logo: "",
         image: "/assets/projects/Movie_Recommender.jpeg",
         tags: [
@@ -150,53 +150,4 @@ export const experiences = [
     },
     
 ];  
-export const reviews = [
-    {
-        name: "Jack",
-        username: "@jack",
-        body: "I've never seen anything like this before. It's amazing. I love it.",
-        img: "https://robohash.org/jack",
-    },
-    {
-        name: "Jill",
-        username: "@jill",
-        body: "I don't know what to say. I'm speechless. This is amazing.",
-        img: "https://robohash.org/jill",
-    },
-    {
-        name: "John",
-        username: "@john",
-        body: "I'm at a loss for words. This is amazing. I love it.",
-        img: "https://robohash.org/john",
-    },
-    {
-        name: "Alice",
-        username: "@alice",
-        body: "This is hands down the best thing I've experienced. Highly recommend!",
-        img: "https://robohash.org/alice",
-    },
-    {
-        name: "Bob",
-        username: "@bob",
-        body: "Incredible work! The attention to detail is phenomenal.",
-        img: "https://robohash.org/bob",
-    },
-    {
-        name: "Charlie",
-        username: "@charlie",
-        body: "This exceeded all my expectations. Absolutely stunning!",
-        img: "https://robohash.org/charlie",
-    },
-    {
-        name: "Dave",
-        username: "@dave",
-        body: "Simply breathtaking. The best decision I've made in a while.",
-        img: "https://robohash.org/dave",
-    },
-    {
-        name: "Eve",
-        username: "@eve",
-        body: "So glad I found this. It has changed the game for me.",
-        img: "https://robohash.org/eve",
-    },
-];
+
